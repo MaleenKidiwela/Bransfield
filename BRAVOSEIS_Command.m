7d41c32ee779fe1519ebfd3b2d59236094bce8e8
Maleen Kidiwela <seismic@uw.edu>
2020-07-22 00:00:46 +0530

    changes to main directory

--- BRAVOSEIS_Command.m
+++ BRAVOSEIS_Command.m
@@ -4,10 +4,10 @@ srGeometry  = load_srGeometry('srGeometryOrca_v1.mat');
 srStation   = load_srStation('srStationOrcaDeploy_v1.mat',srGeometry);
 srEvent     = load_srEvent('srEventOrcaTomo_v1.mat',srGeometry);
 
-tlPickDir = '/Users/earthnote/Desktop/Bransfield/Stingray/Picks'
+tlPickDir = '/Users/earthnote/Desktop/Brainsfield/Stingray/Picks'
 v= 1456;
 stationIn ={'BRA13','BRA14','BRA15','BRA16','BRA18','BRA19','BRA20','BRA21','BRA22','BRA23','BRA24','BRA25','BRA26','BRA27'}
-%stationIn ={'BRA25'} % to isolate what you want to relocate
+%stationIn ={'BRA13'} % to isolate what you want to relocate
 PhaseIn ={'Pw','Pw','Pw','Pw','Pw','Pw','Pw','Pw','Pw','Pw','Pw','Pw','Pw','Pw'}
 %PhaseIn ={'Pw'}
 channelIn =[]
@@ -23,17 +23,16 @@ run_obsloc_bravoseis;
 
 %% Hitogram of misfits
 
-e= s.time-s.timePred;
+e= s.time-s.timePred
 e= e(:);
 e=e(~isnan(e));
 figure
 histfit(e)
 std(e)
 
-a=sum(~isnan(s.time),2);
+a=sum(~isnan(s.time),2) % Number of station detecting same event
 figure
 histogram(a)
-
 
 %% Plotting event detection 
 
@@ -47,3 +46,4 @@ logic = logical(a > 3);
 plot(s.xEvent(logic),s.yEvent(logic),'.','MarkerSize',12)
 title('Events detected by stations')
 legend('Event Unused','<3','3','>3')
+
